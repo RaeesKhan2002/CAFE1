46b0ec09dd7b921c52fba8e57a4735d7ec71eb64
RaeesKhan2002 <khanraees192002@gmail.com>
2025-12-16 10:10:26 +0900

    Implement unit tests for cafe system functionality
    Added unit tests for menu item creation, order item addition, total calculation, and bill generation.

--- Cafe/unit_test_cafes_system.py
+++ Cafe/unit_test_cafes_system.py
@@ -1,11 +1,15 @@
 import unittest
-from "Cafe.py" import FoodItem, DrinkItem, Order, Customer, Bill
+from Cafe import FoodItem, DrinkItem, Order, Customer, Bill
 
 class TestCafeSystem(unittest.TestCase):
+
+    # Defines the test class which groups all the test cases for the system
 
     def test_menu_item_creation(self):
         item = FoodItem("Sandwich", 5.00, "Carbs")
         self.assertEqual(item.getPrice(), 5.00)
+
+        # Creates the customers orders/drinks
 
     def test_order_add_item(self):
         customer = Customer("Thawhid")
@@ -14,12 +18,16 @@ class TestCafeSystem(unittest.TestCase):
         order.addItem(item)
         self.assertEqual(len(order.orderItems), 1)
 
+        #adds multiple items to the order and calculates the prices
+
     def test_order_total_calculation(self):
         customer = Customer("Arshad")
         order = Order("2", customer)
         order.addItem(FoodItem("Salad", 3.90, "Healthy"))
         order.addItem(DrinkItem("Tea", 1.80, "Medium"))
         self.assertAlmostEqual(order.calculateTotal(), 5.70)
+
+        # Generates a bill for the customer, also confirms the total amount of the order after tax
 
     def test_bill_calculation(self):
         customer = Customer("Jawad")
